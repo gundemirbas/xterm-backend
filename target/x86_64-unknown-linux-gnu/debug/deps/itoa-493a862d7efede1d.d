91e0e50b3e33c530e89d6cf22d09f00faecd25ef
default <gundemirbas@gmail.com>
2025-11-27 00:57:56 +0000

    Stop tracking target/ and honor .gitignore

--- target/x86_64-unknown-linux-gnu/debug/deps/itoa-493a862d7efede1d.d
+++
@@ -1,8 +0,0 @@
-/home/coder/xterm-backend/target/x86_64-unknown-linux-gnu/debug/deps/itoa-493a862d7efede1d.d: /home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs /home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs
-
-/home/coder/xterm-backend/target/x86_64-unknown-linux-gnu/debug/deps/libitoa-493a862d7efede1d.rlib: /home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs /home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs
-
-/home/coder/xterm-backend/target/x86_64-unknown-linux-gnu/debug/deps/libitoa-493a862d7efede1d.rmeta: /home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs /home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs
-
-/home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs:
-/home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs:
--- target/x86_64-unknown-linux-gnu/release/deps/itoa-73348fbfe5aeb737.d
+++
@@ -1,8 +0,0 @@
-/home/coder/xterm-backend/target/x86_64-unknown-linux-gnu/release/deps/itoa-73348fbfe5aeb737.d: /home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs /home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs
-
-/home/coder/xterm-backend/target/x86_64-unknown-linux-gnu/release/deps/libitoa-73348fbfe5aeb737.rlib: /home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs /home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs
-
-/home/coder/xterm-backend/target/x86_64-unknown-linux-gnu/release/deps/libitoa-73348fbfe5aeb737.rmeta: /home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs /home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs
-
-/home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs:
-/home/coder/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs:
